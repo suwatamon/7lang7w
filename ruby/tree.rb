e0653104fc747df1eeea04888604a8e601af1dce
Suwa, Tamon <suwatamon@gmail.com>
2019-09-08 08:25:48 +0900

    added tree_hash in tree.rb

--- ruby/tree.rb
+++ ruby/tree.rb
@@ -2,6 +2,7 @@
 class Tree
     attr_accessor :children, :node_name
     def initialize(name, children=[])
+        children.each {|c| puts c.node_name}
         @children = children
         @node_name = name
     end
@@ -16,10 +17,16 @@ class Tree
     end
 end
 
+tree_hash = 
+{'grandpa' => 
+    {'dad' => {'child 1' => [], 'child 2' => []}, 
+     'uncle' => {'child 3' => []}, 'child 4' => []}
+}
 ruby_tree = Tree.new(
     "Ruby", 
     [Tree.new("Reia"), 
      Tree.new("MacRuby")])
+# ruby_tree = Tree.new(tree_hash)
 
 puts "Visiting a node"
 ruby_tree.visit {|node| puts node.node_name}
