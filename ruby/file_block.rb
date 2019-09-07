3dbd5321981374fb0cc0500195ccd31d38fa819d
suwatamon <suwatamon@gmail.com>
2019-09-08 07:16:39 +0900

    impliment simle grep script

--- ruby/file_block.rb
+++ ruby/file_block.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/ruby
+filename = ARGV[0]
+pattern = Regexp.new(ARGV[1])
+i = 0
 
-
-
+File.open(filename, 'r').each_line do |line|
+    i += 1
+    puts("#{i}: #{line}") if line =~ pattern
+end
