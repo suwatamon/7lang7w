62ba555c717e36bc258db2c8dbabad5670a0fbe3
Suwa, Tamon <suwatamon@gmail.com>
2019-09-08 10:11:53 +0900

    Modified to use hash to initialize tree structure

--- ruby/tree.rb
+++ ruby/tree.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/ruby
 class Tree
     attr_accessor :children, :node_name
-    def initialize(name, children=[])
-        children.each {|c| puts c.node_name}
-        @children = children
-        @node_name = name
+    def initialize(hash)
+        @node_name = []
+        @children = []
+        hash.each do |key, value|
+            @node_name.push(key)
+            @children.push(Tree.new(value))
+        end
     end
 
     def visit_all(&block)
@@ -20,13 +23,9 @@ end
 tree_hash = 
 {'grandpa' => 
     {'dad' => {'child 1' => [], 'child 2' => []}, 
-     'uncle' => {'child 3' => []}, 'child 4' => []}
+     'uncle' => {'child 3' => [], 'child 4' => []}}
 }
-ruby_tree = Tree.new(
-    "Ruby", 
-    [Tree.new("Reia"), 
-     Tree.new("MacRuby")])
-# ruby_tree = Tree.new(tree_hash)
+ruby_tree = Tree.new(tree_hash)
 
 puts "Visiting a node"
 ruby_tree.visit {|node| puts node.node_name}
