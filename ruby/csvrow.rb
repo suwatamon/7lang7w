8450cf6c4e85164b77071c4714b5a1a3eabbb720
Suwa, Tamon <suwatamon@gmail.com>
2019-10-14 16:03:49 +0900

    Impliment CsvRow class

--- ruby/csvrow.rb
+++ ruby/csvrow.rb
@@ -1,3 +1,67 @@
 #!/usr/bin/ruby
 module ActsAsCsv
+    def self.included(base)
+        base.extend ClassMethods
+    end
+    
+    module ClassMethods
+        def acts_as_csv
+            include InstanceMethods
+        end
+    end
+
+    class CsvRow
+        def method_missing name, *args
+            row_name = name.to_s
+            @row_contents[row_name]
+        end
+
+        attr_accessor :row_contents
+        def initialize(headers, row)
+            @row_contents = {}
+            i = 0
+            row.chomp.gsub(/\s+/, '').split(',').each do |each_row|
+                @row_contents[headers[i]] = each_row
+                i = i+1
+            end
+        end
+    end
+
+    module InstanceMethods
+        def read
+            @csv_contents = []
+            filename = self.class.to_s.downcase + '.txt'
+            file = File.new(filename)
+            @headers = file.gets.chomp.gsub(/\s+/, '').split(',')
+
+            file.each do |row|
+                @csv_contents << CsvRow.new(@headers, row)
+            end
+        end
+
+        attr_accessor :headers, :csv_contents
+
+        def initialize
+            read
+        end
+
+        def each
+            @csv_contents.each do |row|
+                yield row
+            end
+        end
+    end
 end
+
+class RubyCsv
+    include ActsAsCsv
+    acts_as_csv
+end
+
+m = RubyCsv.new
+# puts 'headers: ', m.headers.inspect
+# puts 'csv_contents: ', m.csv_contents.inspect
+
+# m.each {|row| puts row.row_contents}
+m.each {|row| puts row.one}
+
